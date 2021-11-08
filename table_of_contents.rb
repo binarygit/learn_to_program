93c4c49c377f85ec840cbfddc9f80bbf88aa75e4
binarygit <Inscription.abhi@outlook.com>
2021-11-08 17:04:28 +0545

    finish table_of_contents

---
+++ table_of_contents.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+require 'pry-byebug'
+
+line_width = 50
+
+puts "\n"
+puts "Table of contents".center(50)
+puts "\nChapter 1: Numbers" + "page 1".rjust(line_width/3)
+puts "\nChapter 2: Letters" + "page 5".rjust(line_width/3)
+puts "\nChapter 3: Variables" + "page 9".rjust(line_width/4 + 2)
+puts "\nChapter 4: Classes" + "page 15".rjust(line_width/3 + 1)
+puts "\nChapter 5: Modules" + "page 20".rjust(line_width/3 + 1)
