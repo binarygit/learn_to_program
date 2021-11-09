4b15a29f161c4c0978a9cd2c6361824970862db2
binarygit <Inscription.abhi@outlook.com>
2021-11-09 14:36:01 +0545

    modified table_of_contents to utilize arrays

--- table_of_contents.rb
+++ table_of_contents.rb
@@ -3,10 +3,12 @@ require 'pry-byebug'
 
 line_width = 50
 
-puts "\n"
-puts "Table of contents".center(50)
-puts "\nChapter 1: Numbers" + "page 1".rjust(line_width/3)
-puts "\nChapter 2: Letters" + "page 5".rjust(line_width/3)
-puts "\nChapter 3: Variables" + "page 9".rjust(line_width/4 + 2)
-puts "\nChapter 4: Classes" + "page 15".rjust(line_width/3 + 1)
-puts "\nChapter 5: Modules" + "page 20".rjust(line_width/3 + 1)
+chapters = ["\nChapter 1: Numbers", "\nChapter 2: Letters", "\nChapter 3: Variables", "\nChapter 4: Classes",
+            "\nChapter 5: Modules"]
+page_nums = ['page 1', 'page 5', 'page 9', 'page 15', 'page 20']
+
+i = 0
+while i < chapters.length
+  puts chapters[i] + page_nums[i].rjust(line_width / 3)
+  i += 1
+end
